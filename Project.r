3be83d6abc6280fe21db3a1d2b9dfa85956d3d61
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-06-14 14:06:29 -0400

    Update Project.r

--- Project.r
+++ Project.r
@@ -1,7 +1,14 @@
 
 MyData <- read.csv(file="C:\\Users\\mfatemeh\\Desktop\\R_programming-master\\project\\data set\\bank_1.csv", header=TRUE, sep=",")
 
+###number of the missing 
 sapply(MyData, function(x) sum(is.na(x)))
 
-plot(density(MyData$age))
-plot(density(MyData$duration))
+###Q1- what is the population age of customers that been contacted
+summary(MyData$age) #some statistical information
+tab1=xtabs(MyData$age)
+hist(MyData$age)
+qplot(age, data = MyData, geom = "histogram", fill = housing )
+
+###Q2- the demographic information on the type of the job of the contacted customre
+tab2=table(MyData$job)
